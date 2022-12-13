366a677f9180b1f0f8994610dfa0b0323de6a06b
Anastasia Kharchenko <anastasia.falls@gmail.com>
2022-12-13 14:59:31 +0100

    Search bar op dashboard pagina werkt

--- SDL2/frontend/main.js
+++ SDL2/frontend/main.js
@@ -689,15 +689,23 @@ function myFunction() {
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
+  for (i = 1; i < tr.length; i++) {
+    // td = tr[i].getElementsByTagName("td")[0];
+    var tds = tr[i].getElementsByTagName("td");
+    var found = false;
+    for (j = 0; j < tds.length; j++) {
+      td = tds[j]
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          found = true;
+        }
       }
+    }
+    if (found) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
 }
